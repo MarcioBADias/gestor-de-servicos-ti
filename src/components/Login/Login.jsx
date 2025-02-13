00baf2ad3d5fb561243f3da0366eec1c164f25cf
MarcioBADias <marcio.b.a.dias@gmail.com>
2025-02-13 01:00:38 -0300

    ✔️ Primeira estrutura do form login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { FaUser, FaLock } from 'react-icons/fa'
 
 function Login() {
   return (
     <div>
-      <h1>Gestor de Serviços</h1>
+      <div className="container">
+        <form>
+          <h1>Acesso de usuario</h1>
+          <div className="input-field">
+            <input
+              type="text"
+              name="email"
+              placeholder="E-mail"
+              required
+              value="#"
+            />
+            <FaUser className="icon" />
+          </div>
+          <div className="input-field">
+            <input
+              type="password"
+              name="senha"
+              placeholder="Senha"
+              required
+              value="#"
+            />
+            <FaLock className="icon" />
+          </div>
+          <button type="submit">Login</button>
+          <div className="signup-link">
+            <p>
+              Cadastre-se <a href="#">aqui</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
